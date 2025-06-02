b28a4b1f559f741a423ddbfcc090faa6085f7e0e
rpinhoDGT <159260216+rpinhoDGT@users.noreply.github.com>
2025-06-02 17:24:44 +0100

    Update 1_lasboundaries_lotes.bat

--- lidar2024/batch_laz_copc/1_lasboundaries_lotes.bat
+++ lidar2024/batch_laz_copc/1_lasboundaries_lotes.bat
@@ -3,16 +3,23 @@
 :: Revision: 2025.02.22
 :: License: This code is distributed under AGPL-3.0 (GNU AFFERO GENERAL PUBLIC LICENSE Version 3)
 ::
+:: Script para executar o comando lasboundary64 do lastools em multiplos ficheiros numa lista de pastas/caminhos
+:: https://downloads.rapidlasso.de/html/lasboundary_README.html
+::
 :: Objetivo: Este script cria os limites dos ficheiros laz em shp file no final agrega todos todos num único ficheiro gpkg.
 :: Verifica ainda se o ficheiro gpkg já existe na pasta destino, se existir passa à frente.
+
 
 @echo off
 setlocal enabledelayedexpansion
 
 set boundariesdir=1_boundaries_lotes
-set ncores=16
 set lotelist=1_lasboundaries_lotes_caminho.csv
-
+REM opções de filtragem de classes: -keep_cass ou -drop_class
+REM opção -keep_class 2 - Apenas manter classe 2 = Ground
+REM opção de -v verbose, relatar a execução de cada ficheiro
+set opcoes=-epsg 3763 -keep_class 2 -v
+set ncores=16
 
 for /f "skip=1 usebackq tokens=1,2 delims=," %%a in (%lotelist%) do (
   echo %%a
@@ -21,7 +28,7 @@ for /f "skip=1 usebackq tokens=1,2 delims=," %%a in (%lotelist%) do (
     
     mkdir %boundariesdir%\boundaries_%%a
 	
-    lasboundary64 -epsg 3763 -v -labels -labels_file_name_full -only_2d -cores %ncores% -i %%b\*.laz -odir %boundariesdir%\boundaries_%%a
+    lasboundary64 %opcoes% -labels -labels_file_name_full -only_2d -cores %ncores% -i %%b\*.laz -odir %boundariesdir%\boundaries_%%a
 	
     ogrmerge -single -progress -f GPKG -a_srs EPSG:3763 -o %boundariesdir%\%%a.gpkg %boundariesdir%\boundaries_%%a\*.shp
   )
